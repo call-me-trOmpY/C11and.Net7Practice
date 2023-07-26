954576f38a6c54ebd198127ebc17e7f5ad665f8c
Juan Carlos Alfonso Vina <86676566+call-me-trOmpY@users.noreply.github.com>
2023-07-26 09:47:37 -0500

    Initial commit

---
+++ AsyncConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+HttpClient client = new();
+
+HttpResponseMessage response = await client.GetAsync("https://www.apple.com");
+
+WriteLine( "Apple's home page has {0:N0} bytes.", response.Content.Headers.ContentLength );  
+
+
+/*asdasd*/ 
